878f23771adeea1a5f091a3593fc051f2fc36f42
Tim Olsson <timol085@student.liu.se>
2025-11-13 21:55:02 +0100

    started on lesson 2, vector growth

--- lessons/02_vector_growth_and_invalidations/main.cpp
+++ lessons/02_vector_growth_and_invalidations/main.cpp
@@ -1,14 +1,34 @@
 #include <vector>
 #include <iostream>
+#include <iomanip>
+
 int main(){
     std::vector<int> v;
-    std::cout << "cap=" << v.capacity() << "\n";
-    const int* base=nullptr;
-    for(int i=0;i<30;++i){
+    std::size_t prev_cap = v.capacity();
+    const void* prev_data = nullptr;
+
+    std::cout << "start: size=" << v.size()
+              << " cap=" << v.capacity()
+              << " prev_cap=" << v.data() << "\n";
+    for (int i =0; i < 64; ++i) {
         v.push_back(i);
-        if(base!=&v[0]){
-            std::cout<<"realloc @"<<i<<" old="<<(const void*)base<<" new="<<(const void*)&v[0]<<"\n";
-            base=&v[0];
+
+        if (v.capacity() != prev_cap) {
+            std::cout << "grow @i=" << std::setw(2) << i
+                      << " size=" << std::setw(2) << v.size()
+                      << " cap=" << std::setw(2) << v.capacity()
+                      << " data:" << v.data();
+
+            if (prev_data && prev_data != static_cast<const void *>(v.data()))
+                std::cout << "  (RELOCATED)";
+            std::cout << "\n";
+
+            prev_cap  = v.capacity();
+            prev_data = static_cast<const void *>(v.data());
         }
     }
+
+    std::cout << "final: size=" << v.size()
+              << " cap=" << v.capacity()
+              << " data=" << v.data() << "\n";
 }
